96240d259bfcaf5eff621d07f9d3c616d6b4b1ab
Edmund Home <edmundhome@hotmail.com>
2019-08-26 17:33:53 +0200

    proyecto

---
+++ lucatinder/src/main/java/com/luca/spring/LucatinderApplication.java
@@ -0,0 +1,13 @@
+package com.luca.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucatinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucatinderApplication.class, args);
+	}
+
+}
