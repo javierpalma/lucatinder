d5c6f89ef2485e20de16dcd9f65ccf4fa7ee949e
Edmund Home <edmundhome@hotmail.com>
2019-08-27 14:43:40 +0200

    Generado de documentacion en PerfilDao y PerfilDaoImpl

--- lucatinder/src/main/java/com/luca/spring/dao/PerfilDAO.java
+++ lucatinder/src/main/java/com/luca/spring/dao/PerfilDAO.java
@@ -2,6 +2,14 @@ package com.luca.spring.dao;
 
 import com.luca.spring.model.Perfil;
 
+/**
+ * 
+ * @author Javier Palma
+ * 
+ * Esta Interface sera imlementacion de PerfilDAOImpl
+ *
+ */
+
 public interface PerfilDAO {
 	
 	public void addPerfil(Perfil perfil);
--- lucatinder/src/test/java/com/luca/spring/LucatinderApplicationTests.java
+++ lucatinder/src/test/java/com/luca/spring/LucatinderApplicationTests.java
@@ -5,12 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.luca.spring.model.Perfil;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LucatinderApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void pruebaPerfilDAOImpl() {
+		Perfil p= new Perfil();
+		p.se
+		
 	}
 
 }
