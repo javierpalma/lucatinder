58688a1eff27c5d0d89a5968b66137bf8e739d26
Edmund Home <edmundhome@hotmail.com>
2019-08-27 14:52:42 +0200

    Update PerfilServiceImpl.java
    Imlpementamos la interfaz de PerfilService en PerfilServiceImpl

--- lucatinder/src/main/java/com/luca/spring/Services/PerfilServiceImpl.java
+++ lucatinder/src/main/java/com/luca/spring/Services/PerfilServiceImpl.java
@@ -6,13 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luca.spring.dao.PerfilDAO;
+import com.luca.spring.model.Perfil;
 
 @Service
 @Transactional
-public class PerfilServiceImpl {
+public class PerfilServiceImpl implements PerfilService {
 
 	@Autowired
 	private PerfilDAO perfilDAO;
+
+	@Override
+	public void addPerfil(Perfil perfil) {
+		
+	}
+
+	@Override
+	public void addLike(int idPerfil, int idOtroPerfil) {
+		
+	}
+
+	@Override
+	public Perfil getPefil(int id) {
+		return null;
+	}
 	
 	
 }
