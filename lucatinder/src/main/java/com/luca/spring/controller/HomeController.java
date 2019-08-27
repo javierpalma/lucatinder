85f939c419cdd50948b065765ff02cab2e34ef62
Josemi <josemiguelteruelrebollo@gmail.com>
2019-08-27 17:19:02 +0200

    Update HomeController.java

--- lucatinder/src/main/java/com/luca/spring/controller/HomeController.java
+++ lucatinder/src/main/java/com/luca/spring/controller/HomeController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cliente.spring.domain.Cliente;
 import com.luca.spring.Services.PerfilService;
 import com.luca.spring.model.Perfil;
+
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,13 +38,28 @@ public class HomeController {
 	 * @version 1.0
 	 */
 	  @RequestMapping(value = "/addPerfil", method = RequestMethod.POST)
-	  public String processSubmit(@ModelAttribute("perfil") Perfil perfil, ModelMap model) { 
+	  public String altaPerfil(@ModelAttribute("perfil") Perfil perfil, ModelMap model) { 
 		  logger.info("-- Registrando");
 	      System.out.println("-- metodo addCliente");
 	      System.out.println("--------"+perfil);	      
 	      model.addAttribute(perfil);
 	      perfilService.addPerfil(perfil);
 	      return "listado.html";
+	  }
+	  
+	  /**
+	 * @author Jose Miguel
+	 * @param perfil
+	 * @param model
+	 * @return inicio.html
+	 * @date 27/08/19
+	 * @version 1.0
+	   */
+	  @RequestMapping(value = "/", method = RequestMethod.GET)
+	  public String inicio(@ModelAttribute("perfil") Perfil perfil) {
+		  logger.info("-- en Inicio");	
+
+	      return "inicio.html";
 	  }
 
 }
