6583befe8f705e1b4f9278d17ae7693e0d218bb2
Edmund Home <edmundhome@hotmail.com>
2019-08-27 14:15:03 +0200

    Update PerfilDAOImpl.java

--- lucatinder/src/main/java/com/luca/spring/dao/PerfilDAOImpl.java
+++ lucatinder/src/main/java/com/luca/spring/dao/PerfilDAOImpl.java
@@ -1,5 +1,14 @@
 package com.luca.spring.dao;
 
+import com.luca.spring.model.Perfil;
+
 public class PerfilDAOImpl implements PerfilDAO {
+	
+	
+	
+	public void addPerfil(Perfil perfil) {
+		
+	}
+	
 
 }
