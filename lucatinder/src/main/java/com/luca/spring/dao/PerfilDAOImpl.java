6d58d02a1cedd7819b056f4e6642367a60b06100
Edmund Home <edmundhome@hotmail.com>
2019-08-27 14:18:02 +0200

    Update PerfilDAOImpl.java

--- lucatinder/src/main/java/com/luca/spring/dao/PerfilDAOImpl.java
+++ lucatinder/src/main/java/com/luca/spring/dao/PerfilDAOImpl.java
@@ -1,11 +1,22 @@
 package com.luca.spring.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
 import com.luca.spring.model.Perfil;
 
+@Repository
 public class PerfilDAOImpl implements PerfilDAO {
+	@PersistenceContext
+	private EntityManager entityManager;
 	
-	
-	
+	public PerfilDAOImpl() {
+		super();
+	}
+
+
 	public void addPerfil(Perfil perfil) {
 		
 	}
