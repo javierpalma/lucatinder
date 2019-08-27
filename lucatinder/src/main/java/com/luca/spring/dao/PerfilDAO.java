f25dc10cc0b37613283c5841e55367c7bf5b5921
Edmund Home <edmundhome@hotmail.com>
2019-08-27 14:16:29 +0200

    Cambios en PerfilDAOImpl & PerfilDao

--- lucatinder/src/main/java/com/luca/spring/dao/PerfilDAO.java
+++ lucatinder/src/main/java/com/luca/spring/dao/PerfilDAO.java
@@ -6,5 +6,5 @@ public interface PerfilDAO {
 	
 	public void addPerfil(Perfil perfil);
 	public void addLike(int idPerfil, int idOtroPerfil);
-
+	public Perfil getPefil(int id);
 }
--- lucatinder/src/main/java/com/luca/spring/dao/PerfilDAOImpl.java
+++ lucatinder/src/main/java/com/luca/spring/dao/PerfilDAOImpl.java
@@ -9,6 +9,18 @@ public class PerfilDAOImpl implements PerfilDAO {
 	public void addPerfil(Perfil perfil) {
 		
 	}
+
+	
+	public void addLike(int idPerfil, int idOtroPerfil) {
+		
+	}
+
+
+	@Override
+	public Perfil getPefil(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
