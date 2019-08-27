12feb25d8eee46a976eb6c197512b11b49424465
Edmund Home <edmundhome@hotmail.com>
2019-08-27 16:32:41 +0200

    Update HomeController.java

--- lucatinder/src/main/java/com/luca/spring/controller/HomeController.java
+++ lucatinder/src/main/java/com/luca/spring/controller/HomeController.java
@@ -2,6 +2,10 @@ package com.luca.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.luca.spring.Services.PerfilService;
 import org.slf4j.Logger;
@@ -13,6 +17,11 @@ public class HomeController {
 	private PerfilService perfilService;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	
+	@RequestMapping(value = "/addLike", method = RequestMethod.POST)
+	public String addLike(@ModelAttribute("idPerfil") int idPerfil, @ModelAttribute("idOtroPerfil") int idOtroPerfil, ModelMap model) {
+		logger.info("-------DAR LIKE-------");
+		perfilService.addLike(idPerfil, idOtroPerfil);
+		return "listado.html";
+	}
 
 }
