4412c400f6ff867a04f0c4a01946b361e68e340c
Josemi <josemiguelteruelrebollo@gmail.com>
2019-08-28 14:32:35 +0200

    f

--- lucatinder/src/main/java/com/luca/spring/controller/HomeController.java
+++ lucatinder/src/main/java/com/luca/spring/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -110,6 +111,7 @@ public class HomeController {
 		  	return "inicio";
 		  }
 	  }
+	 
 	  
 	  /**
 	   * Metodo genera perfiles falsos.
