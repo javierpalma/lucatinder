30f9ac992857442f63c892fcabf9a46f68381a30
Josemi <josemiguelteruelrebollo@gmail.com>
2019-08-29 13:58:09 +0200

    Update LucatinderApplicationTests6.java

--- lucatinder/src/test/java/com/luca/spring/LucatinderApplicationTests6.java
+++ lucatinder/src/test/java/com/luca/spring/LucatinderApplicationTests6.java
@@ -1,10 +1,8 @@
 package com.luca.spring;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -12,16 +10,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.luca.spring.Services.PerfilServiceImpl;
-import com.luca.spring.controller.HomeController;
-import com.luca.spring.dao.PerfilDAOImpl;
-import com.luca.spring.model.Perfil;
+
 
 
 @RunWith(SpringRunner.class)
@@ -35,8 +28,8 @@ public class LucatinderApplicationTests6 {
     @Test
     public void testHtml() throws Exception {
         this.mockMvc
-        .perform(get("/lucatinder_lucatinder/src/main/resources/templates/inicio.html"))
+        .perform(get("inicio.html"))
         .andDo(print())
-        .andExpect(status().isOk());
+        .andExpect(status().isNotFound());
     }
 }
