ab4fc3710ff40cc74da9f0e9f7d7fd796044a835
Josemi <josemiguelteruelrebollo@gmail.com>
2019-08-31 12:38:04 +0200

    Update RestHomeController.java

--- lucatinder/src/main/java/com/luca/spring/controller/RestHomeController.java
+++ lucatinder/src/main/java/com/luca/spring/controller/RestHomeController.java
@@ -6,14 +6,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
