3d237234fd72714fa0d6fae75495afd22e0a88e1
Edmund Home <edmundhome@hotmail.com>
2019-09-03 18:37:06 +0200

    cambios

--- PruebasAngular/src/app/app-routing.module.ts
+++ PruebasAngular/src/app/app-routing.module.ts
@@ -3,11 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 //Nuevo
 
-import { AddUserComponent } from './add-user/add-user.component';
 
 //Creo las rutas
 const routes: Routes = [
-  { path: 'add', component: AddUserComponent }
+  
 ];
 
 //Todo esto ya estaba al ppio
--- PruebasAngular/src/app/app.module.ts
+++ PruebasAngular/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PerfilComponent } from './perfil/perfil.component';
-import { AddUserComponent } from './add-user/add-user.component';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PerfilComponent,
-    AddUserComponent
+   
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
